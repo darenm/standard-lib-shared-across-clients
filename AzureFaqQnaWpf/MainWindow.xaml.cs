59c559e8f522375909dfe4925a61a16b2e8ad800
Daren May <daren.may@gmail.com>
2017-11-30 14:02:27 -0700

    Add project files.

---
+++ AzureFaqQnaWpf/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using AzureQnaService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AzureFaqQnaWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AzureFaqQuestion _client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _client = new AzureFaqQuestion();
+        }
+
+        private async void AskQuestion_Click(object sender, RoutedEventArgs e)
+        {
+            AskQuestion.IsEnabled = false;
+            AskQuestionLabel.Visibility = Visibility.Hidden;
+            AskQuestionProgress.Visibility = Visibility.Visible;
+            AskQuestionProgress.IsIndeterminate = true;
+            try
+            {
+
+                //await GetOneAnswer();
+                await Get3Answers();
+            }
+            catch (Exception exception)
+            {
+                Response.Children.Insert(0,
+                    new TextBlock
+                    {
+                        Text = exception.Message,
+                        TextWrapping = TextWrapping.Wrap,
+                        Foreground = new SolidColorBrush(Colors.Red)
+                    });
+            }
+            AskQuestion.IsEnabled = true;
+            AskQuestionLabel.Visibility = Visibility.Visible;
+            AskQuestionProgress.Visibility = Visibility.Hidden;
+            AskQuestionProgress.IsIndeterminate = false;
+        }
+
+        private async Task GetOneAnswer()
+        {
+            Response.Children.Insert(0, new TextBlock { Text = await _client.GetOneAnswer(SearchQuery.Text), TextWrapping = TextWrapping.Wrap });
+        }
+
+        private async Task Get3Answers()
+        {
+            var response = await _client.GetTopXAnswer(SearchQuery.Text, 3);
+            foreach (var answer in response.Answers)
+            {
+                Response.Children.Insert(0, new TextBlock { Text = answer.Answer, TextWrapping = TextWrapping.Wrap });
+            }
+        }
+    }
+}
---
+++ AzureQnaService/AzureFaqQuestion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace AzureQnaService
+{
+    public class AzureFaqQuestion
+    {
+        private const string AppId = "8f3f7aec-49cb-4071-820f-8400615f0374";
+        private const string SubscriptionKey = "cefe1fe2f8764367aaeb21804705b241";
+
+        private static readonly HttpClient Client;
+
+        static AzureFaqQuestion()
+        {
+            Client = new HttpClient();
+            Client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+        }
+        public async Task<string> GetOneAnswer(string question)
+        {
+            var questionResponse = await GetTopXAnswer(question, 1);
+            return questionResponse.Answers[0].Answer;
+        }
+
+        public async Task<QuestionResponse> GetTopXAnswer(string question, int top)
+        {
+            var uri = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/" + AppId + "/generateAnswer";
+
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(uri),
+                Method = HttpMethod.Post,
+                Content = new StringContent(JsonConvert.SerializeObject(new { question, top }), Encoding.UTF8, "application/json")
+            };
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var responseMessage = await Client.SendAsync(request);
+            responseMessage.EnsureSuccessStatusCode();
+            var json = await responseMessage.Content.ReadAsStringAsync();
+            var questionResponse = JsonConvert.DeserializeObject<QuestionResponse>(json);
+            return questionResponse;
+        }
+    }
+
+    public class QuestionResponse
+    {
+        public AnswerItem[] Answers { get; set; }
+    }
+
+    public class AnswerItem
+    {
+        public string Answer { get; set; }
+        public double Score { get; set; }
+    }
+
+}
